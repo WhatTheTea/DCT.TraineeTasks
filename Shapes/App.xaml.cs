2fa3cd48473819d0259e3ab59569ea0e450da1ce
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-06 19:00:19 +0200

    Add stylecop

---
+++ Shapes/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="App.xaml.cs" company="Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies. All rights reserved.
+// </copyright>
+
+namespace DCT.TraineeTasks.Shapes;
+
+/// <summary>
+///     Interaction logic for App.xaml.
+/// </summary>
+public partial class App
+{
+    // .
+}
---
+++ Shapes/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+// <copyright file="AssemblyInfo.cs" company="Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies. All rights reserved.
+// </copyright>
+
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, // where theme specific resource dictionaries are located.
+    ResourceDictionaryLocation.SourceAssembly) // where the generic resource dictionary is located.
+]
+
+[assembly: CLSCompliant(true)]
