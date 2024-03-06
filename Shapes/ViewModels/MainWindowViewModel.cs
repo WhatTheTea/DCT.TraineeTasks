eccb5fefecc47b6af0a1e50353889e0fb4705848
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-06 19:40:23 +0200

    Add MainWindowViewModel.cs

---
+++ Shapes/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="MainWindowViewModel.cs" company="Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies. All rights reserved.
+// </copyright>
+namespace DCT.TraineeTasks.Shapes.ViewModels;
+
+using System.Drawing;
+
+
+using ReactiveUI;
+
+public class MainWindowViewModel : ReactiveObject
+{
+    public IEnumerable<Models.Shape> Shapes { get; }
+
+    public Rectangle Boundaries { get; set; } = new(0, 0, 0, 0);
+}
