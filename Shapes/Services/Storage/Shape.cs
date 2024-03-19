07646ef5b64f7a116d3c511f36efc6364fc92c1b
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-19 22:08:26 +0200

    Add shape DTO

---
+++ Shapes/Services/Storage/Shape.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Shape.cs" company="Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies. All rights reserved.
+// </copyright>
+
+using DCT.TraineeTasks.Shapes.Resources;
+
+namespace DCT.TraineeTasks.Shapes.Services.Storage;
+
+public record ShapeDTO(
+    double x,
+    double y,
+    bool isPaused,
+    SupportedShapes kind,
+    int id);
