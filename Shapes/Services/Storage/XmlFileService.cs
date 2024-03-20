73d3988f23b4045f21ebd8581d5cb64a414cc859
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-20 22:33:03 +0200

    Add xml load feature

--- Shapes/Services/Storage/XmlFileService.cs
+++ Shapes/Services/Storage/XmlFileService.cs
@@ -12,17 +12,20 @@ namespace DCT.TraineeTasks.Shapes.Services.Storage;
 public class XmlFileService : IFileService
 {
     public string FileLocation { get; set; } = "movingShapes.xml";
+    private readonly XmlSerializer serializer = new(typeof(ShapeDTO[]), [typeof(ShapeDTO), typeof((double x, double y))]);
 
     public void Save(IEnumerable<ShapeViewModel> shapes)
     {
         var dtos = shapes.Select(x => x.ToDTO());
-        var serializer = new XmlSerializer(typeof(ShapeDTO[]), [typeof(ShapeDTO), typeof((double x, double y))]);
         using var writer = new StreamWriter(this.FileLocation);
-        serializer.Serialize(writer, dtos.ToArray());
+        this.serializer.Serialize(writer, dtos.ToArray());
     }
 
     public IEnumerable<ShapeViewModel> Load()
     {
-        throw new NotImplementedException();
+        using var reader = new StreamReader(this.FileLocation);
+        var dtos = this.serializer.Deserialize(reader) as ShapeDTO[]
+                   ?? throw new FileFormatException(new Uri(this.FileLocation));
+        return dtos.Select(x => x.ToViewModel());
     }
 }
