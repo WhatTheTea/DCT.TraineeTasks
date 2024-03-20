179390d3dd01f8d05e5fe65d3dbebe09994a547e
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-20 19:30:18 +0200

    Add save dialog

--- Shapes/Views/MainWindow.xaml.cs
+++ Shapes/Views/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 // Copyright (c) Digital Cloud Technologies. All rights reserved.
 // </copyright>
 
+using System.Windows;
 using DCT.TraineeTasks.Shapes.ViewModels;
+using Microsoft.Win32;
 
 namespace DCT.TraineeTasks.Shapes.Views;
 
@@ -26,4 +28,19 @@ public partial class MainWindow
     }
 
     public MainViewModel ViewModel => (MainViewModel)this.DataContext;
+
+    private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+    {
+        var dialog = new SaveFileDialog
+        {
+            Filter = "JSON files (*.json)|*.json" 
+            + "|Binary files (*.bin)|*.bin"
+            + "|XML files (*.xml)|*.xml",
+        };
+
+        if (dialog.ShowDialog() ?? false)
+        {
+            Console.WriteLine(dialog.FileName);
+        }
+    }
 }
