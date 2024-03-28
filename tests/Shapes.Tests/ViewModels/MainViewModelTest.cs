0beece54c58137969b4e043adf79a1224da851b6
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-28 11:47:19 +0200

    Add intersection tests

--- tests/Shapes.Tests/ViewModels/MainViewModelTest.cs
+++ tests/Shapes.Tests/ViewModels/MainViewModelTest.cs
@@ -2,14 +2,11 @@
 // Copyright (c) Digital Cloud Technologies. All rights reserved.
 // </copyright>
 
-using System.Runtime.CompilerServices;
-using Accessibility;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using DCT.TraineeTasks.Shapes.Events;
 using DCT.TraineeTasks.Shapes.Resources;
 using DCT.TraineeTasks.Shapes.Services;
 using DCT.TraineeTasks.Shapes.ViewModels;
-using DCT.TraineeTasks.Shapes.Wrappers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
 
@@ -31,30 +28,90 @@ public class MainViewModelTest
         };
     }
     
-    // Intersections
     [Test]
+    [Category("Intersection")]
     public void IntersectionSameShapes()
     {
         // Arrange
         using var vm = this.ViewModel.Monitor();
         var shape1 = new ShapeViewModel(0, 0) { X = 10, Y = 10, };
         var shape2 = new ShapeViewModel(0, 1) { X = 10, Y = 10, };
-        vm.Subject.AddShape(shape1);
-        vm.Subject.AddShape(shape2);
-        vm.Subject.AddEventHandlerToCommand.Execute(shape1);
-        vm.Subject.AddEventHandlerToCommand.Execute(shape2);
+        this.ViewModel.AddShape(shape1);
+        this.ViewModel.AddShape(shape2);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape1);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape2);
+
         // Act
         this.ViewModel.MoveShapes();
 
         // Assert
-        vm.Should().Raise(nameof(vm.Subject.IntersectionOccured))
+        vm.Should().Raise(nameof(MainViewModel.IntersectionOccured))
             .WithArgs<IntersectionEventArgs>(
                 x => x.Shape1 == shape1
                 && x.Shape2 == shape2);
 
-        vm.Should().Raise(nameof(vm.Subject.IntersectionOccured))
+        vm.Should().Raise(nameof(MainViewModel.IntersectionOccured))
             .WithArgs<IntersectionEventArgs>(
                 x => x.Shape1 == shape2
                      && x.Shape2 == shape1);
+    }
+
+    [Test]
+    [Category("Intersection")]
+    public void IntersectionDifferentKinds()
+    {
+        // Arrange
+        using var vm = this.ViewModel.Monitor();
+        var shape1 = new ShapeViewModel(SupportedShapes.Circle, 0) { X = 10, Y = 10, };
+        var shape2 = new ShapeViewModel(SupportedShapes.Square, 1) { X = 10, Y = 10, };
+        this.ViewModel.AddShape(shape1);
+        this.ViewModel.AddShape(shape2);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape1);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape2);
+
+        // Act
+        this.ViewModel.MoveShapes();
+
+        // Assert
+        vm.Should().NotRaise(nameof(MainViewModel.IntersectionOccured));
+    }
+
+    [Test]
+    [Category("Intersection")]
+    public void IntersectionDifferentPositions()
+    {
+        // Arrange
+        using var vm = this.ViewModel.Monitor();
+        var shape1 = new ShapeViewModel(0, 0) { X = 10, Y = 10, };
+        var shape2 = new ShapeViewModel(0, 1) { X = 20, Y = 10, };
+        this.ViewModel.AddShape(shape1);
+        this.ViewModel.AddShape(shape2);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape1);
+        this.ViewModel.AddEventHandlerToCommand.Execute(shape2);
+
+        // Act
+        this.ViewModel.MoveShapes();
+
+        // Assert
+        vm.Should().NotRaise(nameof(MainViewModel.IntersectionOccured));
+    }
+    
+    [Test]
+    [Category("Intersection")]
+    public void PlayButtonPause()
+    {
+        // Arrange
+        var localizedStrings = Ioc.Default.GetService<LocalizerService>() !;
+        localizedStrings.Should().NotBeNull();
+        var vm = this.ViewModel;
+        var shape = new ShapeViewModel(0, 0);
+        vm.AddShape(shape);
+        vm.SelectedShape = shape;
+
+        // Act
+        shape.IsPaused = false;
+
+        // Assert
+        vm.ButtonText.Should().Be(localizedStrings.PlayButtonPause);
     }
 }
