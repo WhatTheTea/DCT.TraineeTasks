505dac097fd3dd1d5ccbf6da60c20bae5cae2455
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-28 17:22:37 +0200

    Add out of bounds exception

---
+++ src/Shapes/Exceptions/ShapeOutOfBoundsException.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file="ShapeOutOfBoundsException.cs" company="Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies. All rights reserved.
+// </copyright>
+
+using System.Text;
+using DCT.TraineeTasks.Primitives;
+using DCT.TraineeTasks.Shapes.ViewModels;
+
+namespace DCT.TraineeTasks.Shapes.Exceptions;
+
+public class ShapeOutOfBoundsException(ShapeViewModel shape)
+    : InvalidOperationException($"{shape.Name} is out of bounds: {new Point()} : {shape.Boundary}!");
