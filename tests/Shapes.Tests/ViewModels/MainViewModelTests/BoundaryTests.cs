14004ab028fb1926cd3eab4b35337d16ffc1f6be
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-29 17:55:44 +0200

    Add canvas updates for children test

---
+++ tests/Shapes.Tests/ViewModels/MainViewModelTests/BoundaryTests.cs
@@ -0,0 +1,25 @@
+﻿using DCT.TraineeTasks.Primitives;
+using DCT.TraineeTasks.Shapes.ViewModels;
+
+namespace DCT.TraineeTasks.Shapes.Tests.ViewModels.MainViewModelTests;
+
+public class BoundaryTests : Base
+{
+    [Test]
+    public void UpdatesForChildren()
+    {
+        // Arrange
+        var vm = this.MonitoredViewModel.Subject;
+        vm.AddShape(new ShapeViewModel(0, 0));
+
+        // Act
+        vm.CanvasHeight = 300;
+        vm.CanvasWidth = 300;
+
+        // Assert
+        vm.Shapes[0]
+            .Boundary
+            .Should()
+            .BeEquivalentTo(new Point(300, 300));
+    }
+}
