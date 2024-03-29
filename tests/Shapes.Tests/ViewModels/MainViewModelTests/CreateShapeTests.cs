5c1e55ea12dd4393cd7078eb06298bf8b6d93bd6
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-29 18:26:19 +0200

    Add create shape command tests

---
+++ tests/Shapes.Tests/ViewModels/MainViewModelTests/CreateShapeTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using DCT.TraineeTasks.Shapes.Resources;
+using DCT.TraineeTasks.Shapes.ViewModels;
+
+namespace DCT.TraineeTasks.Shapes.Tests.ViewModels.MainViewModelTests;
+
+public class CreateShapeTests : Base
+{
+    [Test]
+    public void ShapeExistsAndRaisesCollectionChanged()
+    {
+        // Arrange 
+        var vm = this.MonitoredViewModel.Subject;
+        var collection = vm.Shapes.Monitor();
+
+        // Act
+        vm.CreateShapeCommand.Execute(SupportedShapes.Triangle);
+
+        // Assert
+        collection.Should()
+            .Raise(nameof(ObservableCollection<ShapeViewModel>.CollectionChanged));
+        vm.Shapes.Should().NotBeEmpty();
+    }
+}
