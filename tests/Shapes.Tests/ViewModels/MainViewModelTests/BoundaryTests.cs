b9ce4b80cead33dde3fbdd65c5ef0adc22f86d17
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-03-29 18:04:56 +0200

    Add boundary property test

--- tests/Shapes.Tests/ViewModels/MainViewModelTests/BoundaryTests.cs
+++ tests/Shapes.Tests/ViewModels/MainViewModelTests/BoundaryTests.cs
@@ -5,12 +5,18 @@ namespace DCT.TraineeTasks.Shapes.Tests.ViewModels.MainViewModelTests;
 
 public class BoundaryTests : Base
 {
+    [SetUp]
+    public override void Setup()
+    {
+        base.Setup();
+        this.MonitoredViewModel.Subject.AddShape(new ShapeViewModel(0, 0));
+    }
+
     [Test]
     public void UpdatesForChildren()
     {
         // Arrange
         var vm = this.MonitoredViewModel.Subject;
-        vm.AddShape(new ShapeViewModel(0, 0));
 
         // Act
         vm.CanvasHeight = 300;
@@ -19,6 +25,26 @@ public class BoundaryTests : Base
         // Assert
         vm.Shapes[0]
             .Boundary
+            .Should()
+            .BeEquivalentTo(new Point(300, 300));
+    }
+
+    [Test]
+    public void UpdatesBoundaryProperty()
+    {
+        // Arrange
+        var vm = this.MonitoredViewModel.Subject;
+
+        // Act
+        vm.CanvasHeight = 300;
+        vm.CanvasWidth = 300;
+
+        // Assert
+        this.MonitoredViewModel
+            .Should()
+            .RaisePropertyChangeFor(x => x.CanvasBoundary);
+
+        vm.CanvasBoundary
             .Should()
             .BeEquivalentTo(new Point(300, 300));
     }
