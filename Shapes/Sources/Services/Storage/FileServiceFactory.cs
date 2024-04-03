5c87bae52174e9ae564c009d5abfe4a2ba42f98b
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-03 19:45:50 +0300

    Change keyed singleton to service factory

---
+++ Shapes/Sources/Services/Storage/FileServiceFactory.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file = "FileServiceFactory.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using DCT.TraineeTasks.Shapes.Common;
+
+namespace DCT.TraineeTasks.Shapes.Services.Storage;
+
+public class FileServiceFactory : IFileServiceFactory
+{
+    public IFileService Create(SupportedFileFormats format, string path) => format switch
+    {
+        SupportedFileFormats.Bin => new BinaryFileService { FilePath = path },
+        SupportedFileFormats.JSON => new JsonFileService { FilePath = path },
+        SupportedFileFormats.Xml => new XmlFileService { FilePath = path },
+        _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+    };
+}
---
+++ Shapes/Sources/Services/Storage/IFileServiceFactory.cs
@@ -0,0 +1,12 @@
+﻿// <copyright file = "IFileServiceFactory.cs" company = "Digital Cloud Technologies">
+// Copyright (c) Digital Cloud Technologies.All rights reserved.
+// </copyright>
+
+using DCT.TraineeTasks.Shapes.Common;
+
+namespace DCT.TraineeTasks.Shapes.Services.Storage;
+
+public interface IFileServiceFactory
+{
+    IFileService Create(SupportedFileFormats format, string path);
+}
