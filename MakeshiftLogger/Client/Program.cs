6a55f27498414459aa5bc83369e9b27e6eac5914
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-05 13:27:13 +0300

    Add client code

--- MakeshiftLogger/Client/Program.cs
+++ MakeshiftLogger/Client/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using DCT.TraineeTasks.MakeshiftLogger.Logger;
+
+string[] names = { "Anna", "Viktoriia", "Oleksii", "Oleh" };
+
+await NumbersWithLoggingAsync();
+NamesWithLogging();
+
+return;
+
+void NamesWithLogging()
+{
+    using var logger = new ObjectLogger("names.log");
+    foreach (var name in names) logger.LogInfo(name);
+}
+
+async Task NumbersWithLoggingAsync()
+{
+    await using var logger = new ObjectLogger("int.log");
+    var numbers = Enumerable.Range(1, 100)
+        .Select(x => Random.Shared.Next(-100, 100));
+    await logger.LogInfoAsync(numbers);
+}
