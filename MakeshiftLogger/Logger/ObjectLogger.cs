845980078b06e849d2b5140a23e15bd0d94ab348
Alex Silchenko <silchenko.whatthetea@gmail.com>
2024-04-05 13:00:13 +0300

    Implement disposable interface

---
+++ MakeshiftLogger/Logger/ObjectLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace DCT.TraineeTasks.MakeshiftLogger.Logger;
+
+public class ObjectLogger(string path) : IDisposable, IAsyncDisposable
+{
+    private FileStream LogStream { get; } = new(path, FileMode.Append);
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore();
+        GC.SuppressFinalize(this);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing) LogStream.Dispose();
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        await LogStream.DisposeAsync();
+    }
+}
